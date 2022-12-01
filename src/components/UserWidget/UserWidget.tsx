e855d05f762187f5be2d4494baee6ca948ca5dc9
mykolao <mykolaoleskiv@gmail.com>
2022-12-01 13:33:46 +0200

    feat: UserWidget component

---
+++ src/components/UserWidget/UserWidget.tsx
@@ -0,0 +1,43 @@
+import { FC, useState } from 'react';
+import { UserData } from '../../store/user/types';
+import UserDetails from '../UserDetails/UserDetails';
+import UserList from '../UserList/UserList';
+
+// UserWidget
+interface UserWidgetProps {
+  value: UserData[];
+}
+
+const UserWidget: FC<UserWidgetProps> = ({ value: users }) => {
+  const [selectedUser, setSelectedUser] = useState<UserData | null>(null);
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleHideDetails = () => setShowDetails(false);
+
+  const handleViewDetails = (nickname: string) => {
+    const user = users.find((user) => user.nickname === nickname) || null;
+
+    console.log(user);
+
+    if (user) {
+      setSelectedUser(user);
+      setShowDetails(true);
+    }
+  };
+
+  if (selectedUser && showDetails) {
+    return (
+      <main className="widget-container">
+        <UserDetails value={selectedUser} onClose={handleHideDetails} />
+      </main>
+    );
+  }
+
+  return (
+    <div className="widget-container">
+      <UserList value={users} onViewDetails={handleViewDetails} />
+    </div>
+  );
+};
+
+export default UserWidget;
