faa4278def3e2a76f4e76c78cd21727cb3b2d2f2
mykolao <mykolaoleskiv@gmail.com>
2022-12-01 13:32:43 +0200

    feat: UserItem component

---
+++ src/components/UserItem/UserItem.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { UserData } from '../../store/user/types';
+
+import styles from './UserItem.module.css';
+
+interface UserItemProps {
+  value: UserData;
+  onView: (nickname: string) => void;
+}
+
+const UserItem: FC<UserItemProps> = ({ value, onView }) => {
+  const { photo, name, nickname } = value;
+
+  const handleOnView = () => {
+    onView(nickname);
+  };
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.info}>
+        <img src={photo} alt="user" className={styles.image} />
+        <div className={styles.group}>
+          <h3 className={styles.name}>{name}</h3>
+          <p className={styles.nickname}>{nickname}</p>
+        </div>
+      </div>
+      <button className={styles.button} onClick={handleOnView}>View</button>
+    </div>
+  );
+};
+
+export default UserItem;
